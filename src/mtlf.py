2a8e043b89386576f4fadac2a98c81e79582f376
starling-jane <starling-jane@github.com>
2026-09-24 22:11:21 -0400

    +

--- src/mtlf.py
+++ src/mtlf.py
@@ -82,6 +82,7 @@ class Cards_dict:
 				args.append(m.group(3) + '=' + m.group(4))
 			else:
 				args.append(m.group(1) + '=' + m.group(2))
+        print(args)
 		#args = query_string.split(' ')
 		return self.search_json(args, sort, cardattr)
 	# queries is a dict of key-value pairs
